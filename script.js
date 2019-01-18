8f1f0cd84599b22e61bffb21e6165ca6cee20da3
odipojames <odipojames12@gmai.com>
2019-01-19 00:13:03 +0300

    script.js file

---
+++ script.js
@@ -0,0 +1,35 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("second");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+$(document).ready(function() {
+
+  $('#A').click(function() {
+    $('#three').hide();
+    $('#five').hide();
+    $('#two').show();
+  })
+
+
+// displaying projects
+  $('#B').click(function() {
+    $('#two').hide();
+    $('#three').show();
+  })
+
+// displaying contact
+  $('#C').click(function() {
+    $('#two').hide();
+    $('#three').hide();
+    $('#five').show();
+  })
+
+})
